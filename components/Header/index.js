568989fb624a8e390c340b85754e62121c298bc4
thesoundfromthesky <nagara@naver.com>
2019-11-01 10:16:16 -0500

    header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,24 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  const header = document.createElement("div");
+
+  const date = document.createElement("span");
+  date.classList.add("date");
+  date.textContent = "SMARCH 28, 2019";
+
+  const title = document.createElement("h1");
+  title.textContent = "Lambda Times";
+
+  const temp = document.createElement("span");
+  temp.classList.add("temp");
+  temp.textContent = "98°";
+
+  header.append(date, title, temp);
+
+  return header;
+}
+
+const [headerContainer] = document.getElementsByClassName("header-container");
+headerContainer.appendChild(Header());
